d485acdfd1d2e79efacf223d864bdfd408060b1c
Mark Poepping <mark.poepping@mmpro.de>
2017-12-17 18:39:15 +0100

    Minor fixes for cloudfront location finder + display detected domain if not from CF + typo fix

--- cloudfrontLocation/cloudfrontLocation.sh
+++ cloudfrontLocation/cloudfrontLocation.sh
@@ -8,13 +8,18 @@ set -e
 read -p 'Domain (defaults to media.admiralcloud.com): ' DOMAIN
 DOMAIN=${DOMAIN:=media.admiralcloud.com}
 
-TRC=$(host media.admiralcloud.com | head -1 | perl -pe 'if(($v)=/(([0-9]+([.][0-9]+)+))/){print"$v\n";exit}$_=""')
+TRC=$(host $DOMAIN | head -1 | perl -pe 'if(($v)=/(([0-9]+([.][0-9]+)+))/){print"$v\n";exit}$_=""')
 echo "Detected IP: $TRC"
 
+LOCDOM=$(dig -x $TRC +short)
 LOC=$(dig -x $TRC +short | perl -pe 'if(($v)=/(\w.*)\.(\w{4,6})\./){print"$2\n";exit}$_=""' | tr [a-z] [A-Z])
 #echo "Resolves to $LOC"
 
 PATH='.'$LOC
 L=$($JQP $PATH locations.json)
-echo "AWS CF Edge $LOC is in $L"
+if [ $L="null" ]; then
+  echo "$DOMAIN is delivered via $LOCDOM"
+else
+  echo "$DOMAIN is using AWS CF Edge $LOC in $L"
+fi
 
